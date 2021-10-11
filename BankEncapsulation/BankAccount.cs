c67365765507409de366f5d220fb5fb48dce122a
sdelappe <sdelappe57@gmail.com>
2021-10-11 08:55:27 -0700

    finished exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    class BankAccount
+    {
+        private double balance = 0;
+
+        public double Balance { set { balance = value; } }
+
+        public double Deposit(double deposit)
+        {
+            balance += deposit;
+            return balance;
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine(balance);
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var accnt1 = new BankAccount();
+
+            accnt1.Deposit(2549.77);
+
+            accnt1.GetBalance();
+
+        }
+    }
+}
